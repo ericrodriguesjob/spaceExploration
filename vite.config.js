0e14c61fcd1899f833c62198d4c8593e0ae289d7
Eric Neves <ericnevesrodrigues@gmail.com>
2023-05-28 22:31:54 -0400

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
-export default {
+import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import autoprefixer from 'autoprefixer'
+import { resolve } from 'path'
+
+export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist'
-  }
-}
+  },
+  plugins: [
+    autoprefixer,
+    viteStaticCopy({
+      targets: [
+        {
+          src: resolve(__dirname, './src/views/'),
+          dest: resolve(__dirname, 'dist')
+        }
+      ]
+    })
+  ]
+})

10ef14f607d5d888e3ada3397b7419ced9d21dcc
Eric Neves <ericnevesrodrigues@gmail.com>
2023-05-31 20:46:46 -0400

    Initial commit

--- src/util/ajax.js
+++ src/util/ajax.js
@@ -17,9 +17,6 @@ export async function fetchAPOD({ url, success, error }) {
     const request = await fetch(url)
     const response = await request.json()
     success(response)
-    if (request.status === 200) {
-      boxLoading.classList.remove('active')
-    }
   } catch (err) {
     error(err)
   }

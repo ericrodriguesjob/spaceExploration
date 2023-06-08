bbb2319d8e1c55ef1e5411a02c364da3e1b2df78
Eric Neves <ericnevesrodrigues@gmail.com>
2023-06-08 12:29:31 -0400

    Added AOS library

--- src/util/renderView.js
+++ src/util/renderView.js
@@ -29,7 +29,7 @@ function renderAPIToHTML({ title, date, media_type, url, explanation }) {
     ${(media_type === 'video') ? 
     `<iframe src=${url} scrolling="no" frameborder="0"></iframe>`
     :
-    `<img src="${url}" alt="APOD Image"></img>`
+    `<img data-aos="zoom-in" data-aos-duration="1500" src="${url}" alt="APOD Image"></img>`
     }
     <p>${explanation.split('.')[0]}...</p>
     <a href="https://apod.nasa.gov/apod/astropix.html" target="_blank">Read more...</a>
